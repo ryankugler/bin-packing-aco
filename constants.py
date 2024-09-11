6d107dbadda5f278eb838afc39e9824b999a025e
Ryan <kuglerryan@gmail.com>
2024-09-11 18:46:18 -0400

    separated constants into a file

---
+++ constants.py
@@ -0,0 +1,7 @@
+# Parameters for random rectangles and bin dimensions
+NUM_RECTANGLES = 65  # Number of rectangles to generate
+MAX_RECT_WIDTH = 4   # Maximum width of any rectangle
+MAX_RECT_HEIGHT = 4  # Maximum height of any rectangle
+BIN_WIDTH = 21       # Width of the bin/container where rectangles will be packed
+BIN_HEIGHT = 21      # Height of the bin/container where rectangles will be packed
+NUM_ITERATIONS = 10   # Number of iterations for the Ant Colony Optimization (ACO) algorithm
